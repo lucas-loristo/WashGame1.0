f54c607d77b93fe67c70c67f3351fafc7c4e7202
Lucas Loristo <lucas.loristo@gmail.com>
2018-10-21 19:30:10 +0800

    Wash up may login

---
+++ WashGame1.0-master/Assets/Scripts/Login.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using System;
+
+public class Login : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ WashGame1.0-master/Assets/Scripts/Register.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using System;
+
+public class Register : MonoBehaviour {
+    public GameObject username;
+    public GameObject email;
+    public GameObject password;
+    public GameObject confPassword;
+    private string Username;
+    private string Email;
+    private string Password;
+    private string ConfPassword;
+    private string forms;
+    private bool EmailInvalid = false;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Username = username.GetComponent <InputField>().text;
+        Email = email.GetComponent<InputField>().text;
+        Password = password.GetComponent<InputField>().text;
+        ConfPassword = confPassword.GetComponent<InputField>().text;
+    }
+}
